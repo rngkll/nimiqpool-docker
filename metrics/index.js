9ea143f8c4abe71c6769d58c86adb8947c8bbd89
Sven <sven.buschbeck@gmail.com>
2018-05-05 11:54:26 -0600

    dummy impl of metrics/Stats

--- metrics/index.js
+++ metrics/index.js
@@ -1,25 +1,57 @@
-function formatStats(raw) {
-	let formatted = Object.assign({}, raw);
+const mysql = require('mysql2/promise');
 
-	raw.expected = raw.rewards/raw.connected
+class Stats {
 
-	let index = 0, scale = ['H/s', 'kH/s', 'MH/s', 'GH/s', 'TH/s', 'PH/s'];
-	while (formatted.hashpower >= 1000) {
-		index++;
-		formatted.hashpower /= 1000;
-	}
-	formatted.hashpower = `${formatted.hashpower} ${scale[index]}`
+    constructor(dbHost, dbPassword) {
+        this._dbHost = dbHost;
+        this._dbPassword = dbPassword;
+    }
 
-	return { raw, formatted };
+    async update() {
+        let data = await this.load();
+        data = this.extend(data);
+        data = this.format(data);
+        return data;
+    }
+
+    async load() {
+        if (this._dbHost && !this.connectionPool) {
+            this._connectionPool = await mysql.createPool({
+                host: dbHost,
+                user: 'pool_metrics',
+                password: dbPassword,
+                database: 'pool'
+            });
+        }
+
+        if (this._connectionPool) {
+            // query DB
+        }
+        else return DUMMY_DATA; // testing only
+    }
+
+    extend(data) {
+        data.expected = data.rewards/data.connected;
+        return data;
+    }
+
+    format(raw) {
+        let formatted = Object.assign({}, raw);
+
+        let index = 0, scale = ['H/s', 'kH/s', 'MH/s', 'GH/s', 'TH/s', 'PH/s'];
+        while (formatted.hashpower >= 1000) {
+            index++;
+            formatted.hashpower /= 1000;
+        }
+        formatted.hashpower = `${formatted.hashpower} ${scale[index]}`
+
+        return { raw, formatted };
+    }
 }
 
-function loadStats() {
-	return {
-		// currently
-		connected: 100,
-		// per day
-		rewards: 4000,
-		// current H/s
-		hashpower: 1000000,
-	}
+const DUMMY_DATA = {
+    "connected": 100,     // currently
+    "hashpower": 1000000, // current H/s
+    "fee": 0.01,          // fraction
+    "rewards": 4000       // per day
 }
